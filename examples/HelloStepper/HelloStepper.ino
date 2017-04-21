1a03fba2a5b62b2a53a67fdc1735239dc5e5f8f6
Lutz Niggl <lutz@niggl-schlagbauer.de>
2017-04-21 06:12:39 +0200

    Update HelloStepper.ino

--- examples/HelloStepper/HelloStepper.ino
+++ examples/HelloStepper/HelloStepper.ino
@@ -18,15 +18,15 @@
 
 #include <StepControl.h>
 
-Stepper motor(2, 3);
-StepControl<> controller;
+Stepper motor(2, 3);         // STEP pin: 2, DIR pin: 3
+StepControl<> controller;    // Use default settings 
 
 void setup(){
 }
 
 void loop() 
 {
-  motor.setTargetRel(1000);
-  controller.move(motor);  
+  motor.setTargetRel(1000);  // Set target position to 1000 steps from current position
+  controller.move(motor);    // Do the move
   delay(500);
 }
