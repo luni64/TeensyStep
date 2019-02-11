fea12eb38513a1ed16079ef7f077fc6f31ddfe67
Lutz Niggl <lutz@niggl-schlagbauer.de>
2019-02-11 22:59:41 +0100

    Update PeriodicFunctions.h

--- examples/PeriodicFunctionFollower/PeriodicFunctions.h
+++ examples/PeriodicFunctionFollower/PeriodicFunctions.h
@@ -19,14 +19,13 @@ class RoseFunction
 
     inline float spindleFunction(float t)
     {        
-        theta = omega * t; // constant revolution
-        //Serial.printf("%f %f\n",t, theta);
+        theta = omega * t; // constant revolution        
         return theta;
     }
 
     inline float slideFunction(float t)
     {
-        float phi = fmodf(k * theta, twoPi);
+        float phi = fmodf(k * theta, twoPi);  // we use the theta which was calculated by the spindle function
         return cos(phi);
     }
 
@@ -47,3 +46,43 @@ class RoseFunction
     float theta;
     float omega = twoPi / 60.0f; // default 1rpm
 };
+
+
+// class Epicycloid
+// {
+//   public:
+//     Epicycloid(PathStepper &spindle, PathStepper &slide)
+//     {
+//         spindle.attachFunction([this](float t) { return spindleFunction(t); });
+//         spindle.setScale(1000);
+
+//         slide.attachFunction([this](float t) { return slideFunction(t); });
+//         slide.setScale(1000);
+//     }
+
+//     inline float spindleFunction(float t)
+//     {        
+//         //tbd
+//     }
+
+//     inline float slideFunction(float t)
+//     {
+//         //tbd
+
+//     inline RoseFunction &setParameters(unsigned n, unsigned d)
+//     {
+//         k = (float)n / d;
+//         return *this;
+//     }
+
+//     inline RoseFunction &setRPM(float rpm)
+//     {
+//         omega = rpm * twoPi / 60.0f; // circular frequency in rad/s
+//         return *this;
+//     }
+
+//   protected:
+//     float k;
+//     float theta;
+//     float omega = twoPi / 60.0f; // default 1rpm
+// };
