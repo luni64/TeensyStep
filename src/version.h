0ad207815e23286de25556efb89e20a20e4def54
Zombori Dániel <rockerm4nhun@gmail.com>
2019-12-08 19:17:15 +0100

    Raise version to V0.2.0

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 namespace STM32Step
 {
 
-constexpr const char *version = "V0.1.0";
+constexpr const char *version = "V0.2.0";
 constexpr const char *branch = "platform-conversion";
 
 } // namespace STM32Step
