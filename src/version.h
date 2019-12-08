b0ba95973b1c584fe3e3f71732139bb79205cd1f
Zombori Dániel <rockerm4nhun@gmail.com>
2019-12-08 19:21:19 +0100

    Merge platform-conversion V0.2.0 into master

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@ namespace STM32Step
 {
 
 constexpr const char *version = "V0.2.0";
-constexpr const char *branch = "platform-conversion";
+constexpr const char *branch = "master";
 
 } // namespace STM32Step
