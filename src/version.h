84463b5c1a7f020d50a665d8e45c4135d6b61a3f
luni64 <lutz.niggl@lunoptics.com>
2020-02-17 20:19:38 +0100

    Update version.h
    Fix bug in version.h

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
+#pragma once
+
 namespace TeensyStep
 {
-
-constexpr const char *version = "V2.1";
-constexpr const char *branch = "devTimer";
-
+    constexpr const char* version = "V2.1";
+    constexpr const char* branch = "devTimer";
 } // namespace TeensyStep
