4482a9bc3fff9160dd50470c10855fd57afbc053
Robin Petereit <robin.petereit@tum.de>
2020-02-24 11:36:59 +0100

    Update ESP32 timer with mux

--- src/timer/esp32/TimerField.h
+++ src/timer/esp32/TimerField.h
@@ -2,7 +2,6 @@
 
 #include "Arduino.h"
 #include <functional>
-// //#include "imxrt.h"
 
 #include "../TF_Handler.h"
 
@@ -20,24 +19,26 @@ public:
 
   inline void stepTimerStart();
   inline void stepTimerStop();
+  inline void setStepFrequency(unsigned f);
+  inline unsigned getStepFrequency() { return timerAlarmRead(stepTimer); }
   inline bool stepTimerIsRunning() const { return stepTimerRunning; }
-  inline void setStepFrequency(unsigned f) { timerAlarmWrite(stepTimer, 1. / f, true); }
-  inline unsigned getStepFrequency() { return 0; }
 
   inline void accTimerStart() { timerAlarmEnable(accTimer); }
   inline void accTimerStop() { timerAlarmDisable(accTimer); }
   inline void setAccUpdatePeriod(unsigned period) { timerAlarmWrite(accTimer, period, true); }
 
-  inline void setPulseWidth(unsigned delay) { timerAlarmWrite(stepTimer, delay, false); }
-  inline void triggerDelay() { timerAlarmEnable(delayTimer); }
+  inline void triggerDelay() { timerAlarmEnable(pulseTimer); }
+  inline void setPulseWidth(unsigned pulseWidth) { timerAlarmWrite(pulseTimer, pulseWidth, false); }
 
+  static portMUX_TYPE timerMux;
 
 protected:
-  static TF_Handler* handler;
+  static TF_Handler *handler;
   hw_timer_t *stepTimer;
   hw_timer_t *accTimer;
-  hw_timer_t *delayTimer;
+  hw_timer_t *pulseTimer;
   bool stepTimerRunning;
+  unsigned lastF;
 };
 
 // IMPLEMENTATION ====================================================================
@@ -45,16 +46,17 @@ protected:
 TimerField::TimerField(TF_Handler *_handler)
     : stepTimer(timerBegin(0, 80, true)),
       accTimer(timerBegin(1, 80, true)),
-      delayTimer(timerBegin(2, 80, true)),
-      stepTimerRunning(false)
+      pulseTimer(timerBegin(2, 80, true)),
+      stepTimerRunning(false),
+      lastF(0)
 {
   handler = _handler;
-  timerAttachInterrupt(stepTimer,[]{handler->stepTimerISR();}, true);
+  timerAttachInterrupt(stepTimer, [] { handler->stepTimerISR(); }, true);
   timerAlarmWrite(stepTimer, 1, true);
-  timerAttachInterrupt(accTimer, []{handler->accTimerISR();}, true);
+  timerAttachInterrupt(accTimer, [] { handler->accTimerISR(); }, true);
   timerAlarmWrite(accTimer, 1, true);
-  timerAttachInterrupt(delayTimer, []{handler->pulseTimerISR();}, true);
-  timerAlarmWrite(delayTimer, 1, false);
+  timerAttachInterrupt(pulseTimer, [] { handler->pulseTimerISR(); }, true);
+  timerAlarmWrite(pulseTimer, 1, false);
 }
 
 void TimerField::stepTimerStart()
@@ -69,6 +71,14 @@ void TimerField::stepTimerStop()
   stepTimerRunning = false;
 }
 
+void TimerField::setStepFrequency(unsigned f)
+{
+  if (f != 0)
+    timerAlarmWrite(stepTimer, 1000000 / f, true); // Timer runs at 1 microsecond interval
+  else
+    stepTimerStop();
+}
+
 bool TimerField::begin()
 {
   Serial.println("begin");
@@ -77,5 +87,7 @@ bool TimerField::begin()
 
 void TimerField::end()
 {
-  //TimerControl::end();
+  timerAlarmDisable(stepTimer);
+  timerAlarmDisable(accTimer);
+  timerAlarmDisable(pulseTimer);
 }
