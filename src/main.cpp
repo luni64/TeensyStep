5865d6fbeb79969d3ccc5f72cc95741b119befc9
Erik tideman <erik.tideman@gmail.com>
2020-03-27 13:14:59 +0100

    Changed test program to make it more clear to use with logic analyzer

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,29 @@
 Stepper motor(PIN_A1, PIN_A2);       // STEP pin: 2, DIR pin: 3
 StepControl controller;    // Use default settings
 
-bool led_status = false;
-
 void setup(){
     Serial.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(STATUS_PIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, led_status);
-    digitalWrite(STATUS_PIN, led_status);
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(STATUS_PIN, LOW);
+
+    #ifdef TEENSY
+    Serial.println("TEENSY");
+    #else
+    Serial.println("STM32F4");
+    #endif
 }
 
 void loop(){
-    led_status = !led_status;
-    digitalWrite(LED_BUILTIN, led_status);
-    digitalWrite(STATUS_PIN, led_status);
-    motor.setTargetRel(1);  // Set target position to 1000 steps from current position
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(STATUS_PIN, HIGH);
+
+    motor.setTargetRel(10);  // Set target position to 1000 steps from current position
     controller.move(motor);    // Do the move
+    while(controller.isRunning());
+
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(STATUS_PIN, LOW);
 }
